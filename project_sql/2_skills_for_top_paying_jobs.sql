830e8458d2c101626fda8b080171da2eb1d9293b
Emily Bajrami <emilybajrami22@outlook.com>
2025-10-20 17:15:13 +0100

    sql files

--- project_sql/2_skills_for_top_paying_jobs.sql
+++ project_sql/2_skills_for_top_paying_jobs.sql
@@ -18,7 +18,7 @@ WITH top_paying_jobs AS (
         ON job_postings_fact.company_id = company_dim.company_id
     WHERE
         job_title_short = 'Data Analyst' AND
-        job_location = 'Anywhere' AND
+        job_location = 'United Kingdom' AND
         salary_year_avg IS NOT NULL
     ORDER BY
         salary_year_avg DESC
